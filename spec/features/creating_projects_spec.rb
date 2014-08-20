17ed95c3a8ccdcc875533083b686ef4dda2e6e7a
Taylor Dolezal <onlydole@gmail.com>
2014-08-20 14:39:02 -0400

    Add title functionality for show page

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -15,6 +15,11 @@ feature 'Creating Projects' do
 
     expect(page).to have_content('Project has been created.')
 
+    project = Project.where(name: "TextMate 2").first
+    expect(page.current_url).to eql(project_url(project))
+    title = "TextMate 2 - Projects - Ticketee"
+    expect(page).to have_title(title)
+
   end
   
 end
